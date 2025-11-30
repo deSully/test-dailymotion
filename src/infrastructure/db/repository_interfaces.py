cd05d897d97af5a93d6288b24a48f205b9b3eae1
Williams De Souza <williamsko89@gmail.com>
2025-11-30 18:38:21 +0100

    feat(repo) : implement pgsql repositoryinterface

---
+++ src/infrastructure/db/repository_interfaces.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+from uuid import UUID
+
+from src.core.models import User, ActivationToken
+
+class AbstractUserRepository(ABC):
+    @abstractmethod
+    def create_user(self, user: User) -> User:
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_email(self, email: str) -> Optional[User]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_user_status(self, user_id: UUID, status: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_user_id_and_code(self, user_id: UUID, code: str) -> Optional[ActivationToken]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def delete_activation_token(self, user_id: UUID) -> bool:
+        raise NotImplementedError
